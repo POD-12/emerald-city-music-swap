feb10c071708fa58b14f5951d57a6e973b06d84b
Dilberovicka31 <dilberovicm36@gmail.com>
2021-03-12 20:02:19 -0800

    reinstalled react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import {BrowserRouter as Router ,Route} from "react-router-dom";
+import Signup from "./pages/Signup"
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Signup from "./pages/Signup.js"
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route exact path="/" component={Signup} />
-      </Router>
-    );
-  }
+function App() {
+  return (
+   <Router>
+     <Route exact path="/" component ={Signup} />
+   </Router>
+  );
 }
 
 export default App;
