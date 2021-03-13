52141e8d37741114eaae9c19d5b04b1e899df461
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2021-03-13 13:32:26 -0800

    added carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import {BrowserRouter as Router ,Route} from "react-router-dom";
-
 import Home from "./pages/Home";
+import NavTabs from "./components/NavTabs"
 
 
 function App() {
   return (
    
    <Router>
+     <NavTabs />
      <Route exact path="/" component ={Home} />
 
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Login from "../components/Login";
-import Signup from "../components/Signup"
+import Carousel from "../components/Carousel"
+// import FeaturedCards from "../components/FeaturedCards"
+// import { MDBRow } from 'mdbreact';
+
 
 function Home () {
     return(
-
-      <Signup/>
+      <div>
+        <Carousel/>
+       
+        {/* <FeaturedCards /> */}
+       
+        </div>
+    
     ) 
 }
 export default Home;
