583c2b964f5b157a5e8f40004dec74d7baa4d2a3
Dilberovicka31 <dilberovicm36@gmail.com>
2021-03-12 16:46:39 -0800

    installed dependencies

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -10,9 +14,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hipsterapp")
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
