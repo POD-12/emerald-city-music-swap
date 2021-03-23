2683dad50f8692df34d49a3ce2b001104d7a5b56
alejandro Brand <alejandrobrand8@gmail.com>
2021-03-22 21:37:37 -0700

    commit to pull from main

--- routes/api/recordRoutes.js
+++ routes/api/recordRoutes.js
@@ -1,1 +1,8 @@
 // Stuff
+const router = require("express").router();
+const recordsController = require("../../controllers/recordsController");
+
+router.route("/api/records")
+    .get(recordsController.findAll)
+    .post(recordsController.create)
+
