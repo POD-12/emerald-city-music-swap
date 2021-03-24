8d34eccc78afa1c4f1c8ec2906db536358ab7893
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2021-03-24 15:14:11 -0700

    added filter to browse

--- client/src/components/BrowseForm/index.js
+++ client/src/components/BrowseForm/index.js
@@ -1,8 +1,46 @@
 import { MDBBtn, MDBCol, MDBContainer, MDBRow} from "mdbreact";
-import React from "react";
+import React,{useState,useEffect} from 'react';
+import BrowseCards from '../../components/BrowseCards';
+import API from "../../utils/API";
 
 
 function BrowseForm() {
+  const [records,setRecords] = useState([])
+  const [filterRecords,setfilterRecords] = useState([])
+  const [inputsObj, setimputsObj] = useState({})
+
+  useEffect(()=>{
+      API.findAllRecords().then(res =>{
+        setRecords(res.data)
+        setfilterRecords(res.data)
+      })
+  },[])
+
+  const handleChange = (event) => {
+    const clone = inputsObj
+    clone[event.target.name] = event.target.value
+    setimputsObj(clone)
+  }
+
+  function handleSubmit(event) {
+    console.log(inputsObj.album)
+    
+    console.log(records)
+    var result = records
+    if (inputsObj.album) {
+     result = result.filter(each => each.recordAlbumName === inputsObj.album)
+    }
+    if (inputsObj.artist) {
+      result = result.filter(each => each.recordArtist === inputsObj.artist)
+     }
+    if (inputsObj.genre) {
+      result = result.filter(each => each.recordGenre === inputsObj.genre)
+     }
+    
+    setfilterRecords(result)
+    console.log(result)
+  }
+
   return (
     <MDBContainer className="mt-4 mb-5">
       <MDBRow className="d-flex justify-content-center">
@@ -13,19 +51,19 @@ function BrowseForm() {
             <MDBRow>
               <MDBCol md="6">
                 <div className="form-group">
-                  <input type="text" className="form-control" placeholder="Artist name or album title " />
+                  <input type="text" className="form-control" placeholder="Artist name" name= "artist" onChange={handleChange}/>
                 </div>
               </MDBCol>
               <MDBCol md="6">
               <div className="form-group">
-                  <input type="text" className="form-control" placeholder="Product Type" />
+                  <input type="text" className="form-control" placeholder="Album" name= "album" onChange={handleChange}/>
                 </div>
               </MDBCol>
             </MDBRow>
             <MDBRow>
               <MDBCol md="6">
                 <div className="form-group">
-                  <input type="text" className="form-control" placeholder="Price" />
+                  <input type="text" className="form-control" placeholder="Genre" name= "genre" onChange={handleChange} />
                 </div>
               </MDBCol>
               <MDBCol md="6">
@@ -36,7 +74,7 @@ function BrowseForm() {
             </MDBRow>
           </form>
           <div className="text-center text-md-center">
-            <MDBBtn type="submit" style={{ borderRadius: "2rem" }}>
+            <MDBBtn type="submit" style={{ borderRadius: "2rem" }} onClick={handleSubmit}>
               Search
             </MDBBtn>
           </div>
@@ -44,10 +82,9 @@ function BrowseForm() {
       </MDBRow>
 
       <MDBRow className="justify-content-center">
-      <MDBCol md="9" className=" shadow-box-example rounded z-depth-1-half md-0 mb-5 ">
-      <p className="h3 text-center mb-3 teal-text">Results Here?</p>
-      {/* list search results?  */}
-      </MDBCol>
+      
+      <BrowseCards records = {filterRecords} />
+      
       </MDBRow>
     </MDBContainer>
     
