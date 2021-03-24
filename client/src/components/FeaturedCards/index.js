34153b440d9cb4104b153c2877f015b40602919c
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2021-03-23 23:21:42 -0700

    updated cards and added genre/condition

--- client/src/components/FeaturedCards/index.js
+++ client/src/components/FeaturedCards/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {  MDBRow, MDBCol, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn } from "mdbreact";
+import {  MDBRow, MDBCol, MDBCardBody, MDBIcon, MDBBtn } from "mdbreact";
 
 
 // {/* we want to be able to have multiple posts two on a row? */}
@@ -11,44 +11,49 @@ const FeaturedCards = (props) => {
           Recent posts
         </h2>
 
-        <MDBRow className="d-flex justify-content-center">
+        <MDBRow className="text-md-left">
 
           {props.records.length > 0 ? props.records.map(record =>{
             return(
-                  <MDBCol lg="5" md="12" className="mb-lg-0 mb-4 p-1 mx-1 shadow-box-example z-depth-1-half">
+              <MDBCol lg="6" md="12" className="mb-5 shadow-box-example z-depth-1-half">
             <div><MDBIcon pull="right" far icon="bookmark" size="2x"/></div>
-            <MDBView className="mb-2">
-            
+            <MDBCol md="4" lg="6" className="pl-0 float-left">
               <img
-                className="img-fluid w-50 mx-auto d-block"
                 src="https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg"
-                alt=""
+                className="mx-auto mb-md-0 mb-4 rounded z-depth-1 img-fluid"
+                tag="img"
+                alt="Sample avatar"
               />
-              <MDBMask overlay="white-slight" />
-            </MDBView>
-            <div className="font-weight-bold ">
-              <h3>Album Name: {record.recordAlbumName} </h3>
-            <div>Artist Name: {record.recordArtist}</div>
-            </div>
+            </MDBCol>
+            <MDBCol md="8" lg="6" className="float-left p-0">
+            <h3 className="font-weight-bold mb-1">{record.recordArtist}</h3>
+            <h4 className="font-weight-bold grey-text mb-1">{record.recordAlbumName}</h4>
+            <h6 className="grey-text">Genre: {record.recordGenre} </h6>
             <div>
-            by <a href="#!" className="font-weight-bold">{record.recordPosterContact}</a>,
-              date posted:{Date.now}
+              by <a href="#!" className="font-weight-bold">{record.recordPosterContact}</a>,
+                date posted:{Date.now}
             </div>
-            <div className="dark-grey-text text-left p-2">
+            <div className="grey-text mt-2">
               {record.recordComments}
             </div>
-            <MDBBtn color="grey" style={{ borderRadius: "2rem" }} size="md">
-              Take me Home
-            </MDBBtn>
+            
+            
+            <MDBCol >
+              <MDBBtn color="grey" className="float-left mt-5" style={{ borderRadius: "2rem" }} size="sm">
+                Take me Home
+              </MDBBtn>
+              <h6 className="grey-text float-right">Condition: {record.condition}</h6>
+            </MDBCol>
+
+            
+            </MDBCol>
+
           </MDBCol>
        
             )
           }):""}
 
-        
-          
         </MDBRow>
-  
   
       </MDBCardBody>
    
