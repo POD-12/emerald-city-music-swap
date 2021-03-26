820905f8d33e01d806bdae984764a441f881d97c
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-26 16:32:17 -0700

    deploying

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 const passport = require("passport");
 
 app.use(passport.initialize());
 // Passport config
-passport.use( require("./config/jwtPassportStrategy") );
+passport.use(require("./config/jwtPassportStrategy"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -23,12 +23,20 @@ app.use(express.static("public"));
 // Send every request to the React app
 // Define any API routes before this runs
 app.use("/api", require("./routes/api"));
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hipsterapp")
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/hipsterapp",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
 
